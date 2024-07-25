ae1dbf259727f1be6acf21535a4dea8f64d7d349
Ubuntu <ubuntu@ip-172-31-16-111.us-east-2.compute.internal>
2024-07-25 01:50:15 +0000

    This is my second commit - Added subtraction script

--- calculator.sh
+++ calculator.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-#
+
 # Addition functionality
 
 a=20
 b=30
 addition=$(($a+$b))
 echo "Addition of two numbers $a  & $b is:  $addition "
+
+# Subtraction functionality
+
+sub=$((b-a))
+echo "Subtraction from $b to $a is: $sub "
+
 
